1f561ef29a605ca09117a7146169c2532eb8577f
Abhishek Patel <Abhi882731@gmail.com>
2021-08-02 07:37:34 -0700

    Updates

---
+++ precache-manifest.73f20d31d42db12658bf89125e73a650.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16f7a9e2068e8a218f7fcd2409861f89",
+    "url": "/torres.github.io/index.html"
+  },
+  {
+    "revision": "cd68d673a8d15eb04c48",
+    "url": "/torres.github.io/static/css/2.8c3c0e8e.chunk.css"
+  },
+  {
+    "revision": "80fb26c3cad79ad119ad",
+    "url": "/torres.github.io/static/css/main.f5d71a1b.chunk.css"
+  },
+  {
+    "revision": "cd68d673a8d15eb04c48",
+    "url": "/torres.github.io/static/js/2.dd554c0c.chunk.js"
+  },
+  {
+    "revision": "80fb26c3cad79ad119ad",
+    "url": "/torres.github.io/static/js/main.c1db324d.chunk.js"
+  },
+  {
+    "revision": "baf5280611df175d1a19",
+    "url": "/torres.github.io/static/js/runtime~main.25320e7f.js"
+  },
+  {
+    "revision": "a4d31128b633bc0b1cc1f18a34fb3851",
+    "url": "/torres.github.io/static/media/Material-Design-Iconic-Font.a4d31128.woff2"
+  },
+  {
+    "revision": "b351bd62abcd96e924d9f44a3da169a7",
+    "url": "/torres.github.io/static/media/Material-Design-Iconic-Font.b351bd62.ttf"
+  },
+  {
+    "revision": "d2a55d331bdd1a7ea97a8a1fbb3c569c",
+    "url": "/torres.github.io/static/media/Material-Design-Iconic-Font.d2a55d33.woff"
+  },
+  {
+    "revision": "26ec3c7d0366e0825d705c6e224a8803",
+    "url": "/torres.github.io/static/media/et-line.26ec3c7d.eot"
+  },
+  {
+    "revision": "2bea85e74624239934653d10a63c88bc",
+    "url": "/torres.github.io/static/media/et-line.2bea85e7.svg"
+  },
+  {
+    "revision": "98126e3e1238b0f3b941ad285320ce28",
+    "url": "/torres.github.io/static/media/et-line.98126e3e.ttf"
+  },
+  {
+    "revision": "b01ff252761958325faab1535c90c87f",
+    "url": "/torres.github.io/static/media/et-line.b01ff252.woff"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/torres.github.io/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/torres.github.io/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/torres.github.io/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/torres.github.io/static/media/slick.f97e3bbf.svg"
+  }
+]);
